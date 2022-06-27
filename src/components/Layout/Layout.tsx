c7984bf339fb4f689c3d1936c43e0b7c71670bc4
loremedda <98110186+loremedda@users.noreply.github.com>
2022-06-27 18:19:47 +0200

    updated at last version of common
    * updated at last version of common
    
    Co-authored-by: USERSAD\esignore <elena.signore@nttdata.com>
    Co-authored-by: loremedda <lorenzo.medda.sa@nttdata.com>

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,20 @@
 import { Grid } from '@mui/material';
 import { Box } from '@mui/system';
 import { Footer, Header } from '@pagopa/selfcare-common-frontend';
+import { useSelector } from 'react-redux';
 import React from 'react';
-import { useUnloadEventLogout, useUnloadEventOnExit } from '@pagopa/selfcare-common-frontend/hooks/useUnloadEventInterceptor';
+import { useUnloadEventOnExit } from '@pagopa/selfcare-common-frontend/hooks/useUnloadEventInterceptor';
+import { userSelectors } from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
 import { ENV } from '../../utils/env';
-
 
 type Props = {
   children?: React.ReactNode;
 };
 
 export default function Layout({ children }: Props) {
-  const onLogout = useUnloadEventLogout() ;
   const onExit = useUnloadEventOnExit();
+  const loggedUser = useSelector(userSelectors.selectLoggedUser);
+
   return (
     <Box
       sx={{
@@ -21,11 +23,24 @@ export default function Layout({ children }: Props) {
         minHeight: '100vh',
       }}
     >
-      <Header withSecondHeader={false} onExitAction={onLogout} />
-      <Grid container>
-        {children}
-      </Grid>
-      <Footer assistanceEmail={ENV.ASSISTANCE.ENABLE ? ENV.ASSISTANCE.EMAIL : undefined} onExit={onExit} />
+      <Header
+        withSecondHeader={false}
+        onExit={onExit}
+        assistanceEmail={ENV.ASSISTANCE.ENABLE ? ENV.ASSISTANCE.EMAIL : undefined}
+        enableLogin={true}
+        loggedUser={
+          loggedUser
+            ? {
+                id: loggedUser ? loggedUser.uid : '',
+                name: loggedUser?.name,
+                surname: loggedUser?.surname,
+                email: loggedUser?.email,
+              }
+            : false
+        }
+      />
+      <Grid container>{children}</Grid>
+      <Footer onExit={onExit} loggedUser={!!loggedUser} />
     </Box>
   );
 }
